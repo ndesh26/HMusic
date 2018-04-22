5f776fd07ef7981624aa61cf454df0e74003f23e
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2018-04-22 14:31:24 +0530

    Add a model for stored entries
    also add a handler and route to display the uploads of a
    user
    
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

---
+++ src/Handler/Uploads.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
+module Handler.Uploads where
+
+import Import
+
+getUploadsR :: UserId -> Handler Html
+getUploadsR userId = do 
+                        storedFiles <- runDB $ getUserUploads userId
+                        defaultLayout $ do
+                            setTitle "Welcome To Yesod!"
+                            $(widgetFile "uploads")
+
+getUserUploads :: UserId -> DB [Entity StoredFile]
+getUserUploads userId = selectList [StoredFileUserId ==. userId] []
